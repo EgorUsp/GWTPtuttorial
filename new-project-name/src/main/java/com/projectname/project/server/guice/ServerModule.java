013236563ed8ef4f89653c360aacf967cab019bf
Egor_Uspenskiy <egoyar95@gmail.com>
2018-12-04 12:37:28 +0300

    test2

---
+++ new-project-name/src/main/java/com/projectname/project/server/guice/ServerModule.java
@@ -0,0 +1,9 @@
+package com.projectname.project.server.guice;
+
+import com.google.inject.AbstractModule;
+
+public class ServerModule extends AbstractModule {
+    @Override
+    protected void configure() {
+    }
+}
