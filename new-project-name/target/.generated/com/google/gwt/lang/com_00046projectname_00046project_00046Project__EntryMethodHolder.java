2f29b1b4341543c02f957304f0491aac3291a02a
Egor_Uspenskiy <egoyar95@gmail.com>
2018-12-14 13:34:39 +0300

    rebuild

--- new-project-name/target/.generated/com/google/gwt/lang/com_00046projectname_00046project_00046Project__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046projectname_00046project_00046Project__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
